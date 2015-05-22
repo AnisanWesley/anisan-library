4b8a1701b065be21c699204febc2db3ee3d2d8f8
Wesley Lemos <anisan_wesley@live.com>
2015-05-22 09:37:18 -0300

    Improvement wizard

--- Javascript/wizard-simple/app.js
+++ Javascript/wizard-simple/app.js
@@ -1,88 +1,69 @@
-var angular = angular || {};
-angular.module('wiz', [])
+(function(angular) {
+
+    wizardFactory.$inject = [];
+
+    angular.module('nddPrint')
+        .factory('wizardFactory', wizardFactory);
+    
+    function wizardFactory() {
+        var index = 0, last = 0, pages;
+
+        var wiz = {
+            setPages: setPages,
+            current: current,
+            setCurrent: setCurrent,
+            next: next,
+            prev: prev
+        };
 
 
-    .controller('wizCtrl', function (wizard) {
- 
-    var wz = this;
-    var current="first";
-    
-    wz.current=function(page) {
-        return page==current;
+        Object.defineProperty(wiz, 'canNext', {
+            get: function() {
+                return index < last;
+            }
+        });
+        Object.defineProperty(wiz, 'canPrev', {
+            get: function() {
+                return index > 0;
+            }
+        });
+
+        Object.defineProperty(wiz, 'isLast', {
+            get: function() {
+                return index == last;
+            }
+        });
+
+        function setPages(initialPages) {
+            pages = initialPages;
+            last = pages.length - 1;
+            pages[0].visited = true;
+        }
+
+        function current() {
+            return pages[index].id;
+        }
+
+        function setCurrent(step) {
+            var indexOf = pages.indexOf(step);
+            if (step.visited)
+                index = indexOf;
+           else if ((pages[indexOf - 1].visited))
+                return next();
+            return current();
+        }
+        function next() {
+            var step = pages[++index];
+            step.visited = true;
+            return step.id;
+        }
+        function prev() {
+            return pages[--index].id;
+        }
+
+        return wiz;
+
     }
-    wz.prev=function () {
-        current = wizard.prev();
-    }
-    wz.next=function () {
-        current = wizard.next();
-    }
-    wz.cantPrev=function(){
-        return !wizard.canPrev;
-    };
-    wz.cantNext=function(){
-        return !wizard.canNext;
-    };
-    wz.isLast=function(){
-        return wizard.isLast;
-    };
-    
-    
-    
-    wizard.setPages([
-    'first',
-        'page2',
-        'page3',
-        'page4',
-        'last',
-    ]);
-    
-    
 
 
-})
-
-
-.factory('wizard',function(){
-    
-    var _index=0, _last=0, _current=0, pages = 0;
-    
-    var wiz = {
-        setPages:setPages,
-        current:current,
-        next:next,
-        prev:prev
-    };
-    
-    
-    Object.defineProperty(wiz,'canNext',{get:function(){
-        return _index < _last;
-    }})
-    Object.defineProperty(wiz,'canPrev',{get:function(){
-        return _index > 0;
-    }})
-    
-    Object.defineProperty(wiz,'isLast',{get:function(){
-        return _index == _last;
-    }})
-    
-    
-    
-    function setPages(pages){
-        _pages = pages;
-        _last = pages.length-1
-    }
-    
-    function current(){
-        return _pages[_index];
-    }
-    function next(){
-        return _pages[++_index];
-    }
-    function prev(){
-        return _pages[--_index];
-    }
-    
-    return wiz;
-     
- })
-;
+}(window.angular))
