3d9943eaa4cabd4f7d0bab8cbcd499c3375781e3
Wesley Lemos <anisan_wesley@live.com>
2015-12-01 18:01:20 -0200

    Create string.js

---
+++ Javascript/Extensions/string.js
@@ -0,0 +1,25 @@
+
+String.prototype.query = function () {
+  // This function is anonymous, is executed immediately and
+  // the return value is assigned to QueryString!
+  var query_string = {};
+  var query = this.split('?')[1];
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+        // If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = decodeURIComponent(pair[1]);
+        // If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+      query_string[pair[0]] = arr;
+        // If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+    }
+  } 
+    return query_string;
+};
+
+console.log("?name=wesley&idade=22".query()) // "{ name : 'wesley', idade: 22}"
